1087b52bdd6d4d80326445ce41aea7bd6257e7e6
Paras <parasmonapara6263@gmail.com>
2020-08-12 14:27:30 +0530

    first commit

---
+++ newslettter.php
@@ -0,0 +1,23 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                            </div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
